1a534ef986b0e4d243e7b44988ec28f3f46db481
Raekwon Adams-Rodgers <adamsrodgers@etsu.edu>
2021-07-26 18:35:41 +0000

    main python file

--- py_files/driver.py
+++ py_files/driver.py
@@ -7,8 +7,9 @@ print('Welcome to the youtube Analyzer')
 print('Written by: Tinsae Dejene, Joel Salguero, and Raekwon Adams-Rodgers')
 time.sleep(2)
 
-print('Here you will choose a youtube channel of the list provided down below and look at some statistics from the channel.')
+print('Here you will choose a youtube channel of the list provided down below.')
 time.sleep(2)
+print('some stats will include:\n')
 print('Total View Count, Total Suscriber Count, The Amount of Videos on The Channel. We also give video data for the top 10 most viewed videos of each channel. Here are\nyour choices below: ')
 print(" 1.lockpickinglawyer\n",  "2.john stossel\n", "3.economics explained\n", "4.louis rossman\n", "5.casually explained\n", "6.economics explained 2\n", "7.kirksicle\n", "8.reasontv\n", "9.doug demuro")
 
@@ -22,15 +23,10 @@ while(choice != 'Exit'):
     channel_id, stats = data.popitem()
     channel_stats = stats['channel_statistics']
     video_stats = stats['video_data']
-    print('Here are the channel stats for' + choice + ': ')
-
-
-    print('Total Views: ', channel_stats['viewCount'])
-
-    print('Subcribers: ', channel_stats['subscriberCount'])
-
-    print('Amount of Videos: ', channel_stats['videoCount'])
-
+    print('\nHere are the channel stats for' + choice + ': ')
+    print('\nTotal Views: ', channel_stats['viewCount'])
+    print('\nSubcribers: ', channel_stats['subscriberCount'])
+    print('\nAmount of Videos: ', channel_stats['videoCount'])
     print('\t\tTop 10 most viewed videos data')
 
     sorted_vids = sorted(video_stats.items(), key=lambda item: int(item[1]['viewCount']), reverse=True)
@@ -45,7 +41,7 @@ while(choice != 'Exit'):
         Duration = vid[1]['duration']
         stats.append([Title, Views, Comments, Likes, Dislikes, Duration])
     
-    df = pd.DataFrame(stats, columns = ['Title', 'Views', 'Comments', 'Likes', 'Dislikes', 'Duration'])
+    df=pd.DataFrame(stats, columns=['Title', 'Views', 'Comments', 'Likes', 'Dislikes', 'Duration'])
     print(df.head(10))
     
     time.sleep(3)
